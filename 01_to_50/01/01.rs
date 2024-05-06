94524c65b93d88ce5f75e5f185b2ca52c67a24a4
christian-the-brewer <brewer.christiang@gmail.com>
2024-05-06 00:00:03 -0400

    completes two sum

---
+++ 01_to_50/01/01.rs
@@ -0,0 +1,28 @@
+// Two Sum: 01 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+use std::collections::HashMap;
+
+fn main() {
+   println!("{:?}",sum_to_target([1,2,3,4].to_vec(), 6)); 
+}
+
+fn sum_to_target (nums: Vec<i32>, target: i32) -> (i32, i32) {
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    let mut complement: i32;
+
+    for (index, number) in nums.into_iter().enumerate() {
+        complement = target - number;
+        println!("number: {} index: {}, complement: {}", &number, &index, &complement);
+        println!("map so far: {:?}", &map);
+        if map.contains_key(&number) {
+            return (index as i32, *map.get(&number).unwrap());
+        } else {
+            map.insert(complement, index as i32);
+        
+        }
+    };
+    println!("{:?}", map);
+    unreachable!();
+}
